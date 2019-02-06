47dfe49203d7c9a0eada081bff762e7a1d373a8e
Jaap <jhpa.rodenburg@gmail.com>
2019-02-06 17:03:09 +0100

    Connect

--- electron-boilerplate/app/assets/js/socket.js
+++ electron-boilerplate/app/assets/js/socket.js
@@ -1,14 +1,20 @@
 
 var socket = io('https://ballrenatest.glitch.me/');
 socket.on('connect', function(){
-  console.log("connected!");
+  console.log("Connected to master server!");
 });
 socket.on('UUID', function(data) {
       console.log(data.message);
      var socketinfo = JSON.parse(data.message)
     })
 socket.on('event', function(data){});
-socket.on('disconnect', function(){});
+socket.on('disconnect', function(){
+    console.log("Connection lost! Trying to reconnect....")
+});
+socket.on('callback', function(data){
+    var answer = JSON.parse(data.message);
+    console.log("Success: " + answer.succes);
+})
 
 function Login(){
     var password = document.getElementById("password").value;
@@ -16,7 +22,6 @@ function Login(){
     console.log("Password: " + document.getElementById("password").value);
     console.log("Username: " + document.getElementById("username").value);
     console.log("Logging in..........");
-    document.getElementById("submit").value = "Logging in..........";
     socket.emit('login', { message: '{ "username": "' + username + '","password": "' + password + '"}'});
 
 }
