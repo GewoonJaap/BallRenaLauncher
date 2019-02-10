23574e53d6fc210b4f3b76a945011b4b30978346
Jaap <jhpa.rodenburg@gmail.com>
2019-02-10 16:35:13 +0100

    release command

--- electron-boilerplate/app/assets/js/home.js
+++ electron-boilerplate/app/assets/js/home.js
@@ -1,5 +1,7 @@
 var open = require("open");
 const loggerhome = require('./assets/js/loggerutil.js')('%c[Main Menu]', 'color: #7289da; font-weight: bold');
+const ElectronTitlebarWindows = require('electron-titlebar-windows');
+const titlebar = new ElectronTitlebarWindows(options)
 
 function OpenLink(url){
     open(url);
