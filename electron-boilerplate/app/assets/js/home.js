74015273099be3c2bd7be7faee62f3164a0bdd4f
Jaap <jhpa.rodenburg@gmail.com>
2019-02-10 16:13:07 +0100

    onclick

--- electron-boilerplate/app/assets/js/home.js
+++ electron-boilerplate/app/assets/js/home.js
@@ -1,2 +1,6 @@
 var open = require("open");
 const loggerhome = require('./assets/js/loggerutil.js')('%c[Main Menu]', 'color: #7289da; font-weight: bold');
+
+function OpenLink(url){
+    open(url);
+}
