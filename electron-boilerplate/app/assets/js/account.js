7123bdf218600abc59d3e425264dc84bed87749b
Jaap <jhpa.rodenburg@gmail.com>
2019-02-14 16:51:17 +0100

    Account

--- electron-boilerplate/app/assets/js/account.js
+++ electron-boilerplate/app/assets/js/account.js
@@ -5,10 +5,21 @@ const loggeraccount = require('./assets/js/loggerutil.js')('%c[Account]', 'color
 
 var username = store.get('unicorn.username');
 var email = emoji.get('x');
+var banned = store.get('unicorn.banned');
+var bannedvar;
+loggeraccount.log("Banned: " + banned)
 if(store.get('unicorn.email') !=undefined){
     loggeraccount.log("Email found!");
     email = store.get('unicorn.email');
 }
 
+if(banned != "false"){
+    bannedvar = emoji.get('heavy_check_mark');
+}
+else if(banned = "false"){
+    bannedvar = emoji.get('x');
+}
+
     document.getElementById('emailadress').innerText = "Email: " + email;
     document.getElementById('username').innerText = "Username: " + username;
+    document.getElementById('banned').innerText = "Banned: " + bannedvar;
--- electron-boilerplate/app/assets/js/backend.js
+++ electron-boilerplate/app/assets/js/backend.js
@@ -2,6 +2,8 @@ var request = require('request');
 const loggerbackend = require('./assets/js/loggerutil.js')('%c[Backend]', 'color: #7289da; font-weight: bold');
 const fs = require('fs');
 var socket = io('https://ballrena-node-backend.herokuapp.com');
+var loginkey = store.get('unicorn.loginkey')
+GetUserStats();
 
 socket.on('connect', function(){
     loggerbackend.log("Connected to master server!");
@@ -15,3 +17,67 @@ socket.on('connect', function(){
   socket.on('disconnect', function(){
     loggerbackend.log("Connection lost! Trying to reconnect....");
 });
+
+
+//API
+
+function BacktoLogin(){
+  storecore.delete('unicorn.username');
+  storecore.delete('unicorn.password');
+  storecore.delete('unicorn.loginkey');
+  const BrowserWindow = remote.BrowserWindow;
+  let win = new BrowserWindow({ titleBarStyle: 'default', width:800, height:600, minWidth: 800, minHeight: 600, frame: false, backgroundColor: '#2e2c29' , title:"BallRena Launcher | Login" })
+  win.loadURL(`file://${__dirname}/login.html`);
+  //Close windows
+  var window = remote.getCurrentWindow();
+  window.close();
+}
+
+function GetUserStats(){
+    
+  var options = {
+    method: "GET",
+    url: 'https://auth.ballrena.ml/api/g_userinfo.php?apireqkey=BallRena&func=login&loginreqkey=' + loginkey,
+    headers: {
+      'User-Agent': 'nodejs request',
+    }
+  }
+
+  request(options, (error, response, body) => {
+      if(!error && response.statusCode == 200){
+          var json = JSON.parse(body)
+          if(json.state == "success"){
+            loggerbackend.log(json);
+            store.delete('unicorn.exp');
+            store.delete('unicorn.id');
+            store.delete('unicorn.ip');
+            store.delete('unicorn.email');
+            store.delete('unicorn.rank');
+            store.delete('unicorn.banned');
+            
+            //Save info
+            store.set('unicorn.exp', json.exp);
+            store.set('unicorn.id', json.id);
+           // store.set('unicorn.ip', json.ip);
+            store.set('unicorn.email', json.email);
+            store.set('unicorn.rank', json.rank);
+            store.set('unicorn.banned', json.banstatus);
+            loggerbackend.log("Saved..")
+
+            //Get page
+            if(document.title = "BallRena Launcher | Account")
+            {
+              loggerbackend.log("Account page")
+            }
+
+          }
+          else{
+            BacktoLogin();
+          }
+      }
+      else{
+        BacktoLogin();
+      }
+  })
+
+}
