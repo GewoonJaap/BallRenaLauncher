9e7e0577f95c6547c598ef2d616d6bc4d2913228
Jaap <jhpa.rodenburg@gmail.com>
2019-02-25 17:53:09 +0100

    download delay

--- electron-boilerplate/app/assets/js/home.js
+++ electron-boilerplate/app/assets/js/home.js
@@ -92,7 +92,7 @@ child(executablePath, function(err, data) {
     console.log('outputDir', outputDir);
     loggerdownload.log('ZipFile', zipFile);
     Downloading = true;
-    setInterval(DownloadMBS,5000)
+    setInterval(DownloadMBS,2500)
 
 
     request
