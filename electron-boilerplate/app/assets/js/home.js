81bf7d9be1b0533aec589304995693556264e16f
Jaap <jhpa.rodenburg@gmail.com>
2019-02-25 19:25:27 +0100

    fix

--- electron-boilerplate/app/assets/js/home.js
+++ electron-boilerplate/app/assets/js/home.js
@@ -109,7 +109,12 @@ child(executablePath, function(err, data) {
         document.getElementById('DownloadButton').innerHTML = "Installed | " + store.get('game.version')
         store.set('unicorn.gameversion', store.get('game.version'));
         store.set('game.installed', "true");
-        
+        if(require("os").platform() == "darwin"){
+          var shell = require('shelljs');
+         // var home = require("os").homedir();
+          shell.chmod('a+x', home + '/Documents/BallRena/Game/MacVersie.app/Contents/MacOS/*')
+         // fs.chmodSync(home + '/Documents/BallRena/Game/MacVersie.app/Contents/MacOS/*', '755');
+        }
 
         //Notification
        if(require('os').platform() == "win32"){
